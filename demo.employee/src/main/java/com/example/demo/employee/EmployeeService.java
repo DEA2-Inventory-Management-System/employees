8f8e332225185f5fa4f856e740ec55c412462f50
sandaru20 <sandaruphotography@gmail.com>
2023-05-24 14:33:19 +0530

    updated EmployeeService

--- demo.employee/src/main/java/com/example/demo/employee/EmployeeService.java
+++ demo.employee/src/main/java/com/example/demo/employee/EmployeeService.java
@@ -1,10 +1,15 @@
 package com.example.demo.employee;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 
 import java.util.List;
 
 public class EmployeeService {
+
+    @Autowired
+    EmployeeRepository employeeRepository;
 
 
     public void saveRequest(EmployeeDto employeeDto) {
@@ -15,11 +20,32 @@ public class EmployeeService {
     }
 
     public ResponseEntity<Object> deleteAllEmployee() {
+        try {
+            employeeRepository.deleteAll();
+            return new ResponseEntity<>(HttpStatus.OK);
+        } catch (Exception e) {
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
     }
 
     public ResponseEntity<Object> deleteEmployeeById(int id) {
     }
 
     public ResponseEntity<EmployeeDto> updateId(int id, EmployeeDto employeeDto) {
+        //check if employee exist in database
+        EmployeeRepository poObj = getEmpRec(empid);
+
+        if (poObj != null) {
+            poObj.setempid(EmployeeDto.getempid());
+            poObj.setname(EmployeeDto.getname());
+            poObj.setaddress(EmployeeDto.getaddress());
+            poObj.setphone(EmployeeDto.getphone());
+            poObj.setage(EmployeeDto.getage());
+            poObj.setdob(EmployeeDto.getdob());
+            poObj.setnic(EmployeeDto.getnic());
+            return new ResponseEntity<>(EmployeeRepository.save(poObj), HttpStatus.OK);
+        }
+
+        return new ResponseEntity<>(HttpStatus.NOT_FOUND);
     }
 }
