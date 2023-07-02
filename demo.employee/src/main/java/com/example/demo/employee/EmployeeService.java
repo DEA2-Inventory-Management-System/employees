1603b1b75cccefa3f24e7a80f41c91d6ce737736
sandaru20 <sandaruphotography@gmail.com>
2023-07-02 20:30:56 +0530

    Edited EmployeeService

--- demo.employee/src/main/java/com/example/demo/employee/EmployeeService.java
+++ demo.employee/src/main/java/com/example/demo/employee/EmployeeService.java
@@ -3,47 +3,59 @@ package com.example.demo.employee;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+
 
 import java.util.List;
+import java.util.Optional;
 
+@Service
 public class EmployeeService {
-
     @Autowired
     EmployeeRepository employeeRepository;
 
 
-    public void saveRequest(EmployeeDto employeeDto) {
+    public void saveEmployee(Employee employee) {
+        employeeRepository.save(employee);
     }
 
-    
-    public List<EmployeeDto> getAllEmployeeRequests() {
+
+    public List<Employee> getEmployee() {
+        return employeeRepository.findAll();
     }
 
-    public ResponseEntity<Object> deleteAllEmployee() {
+    public ResponseEntity<Object> deleteByEmployeeId(int empid) {
         try {
-            employeeRepository.deleteAll();
+            //check if employee exist in database
+            Optional<Employee> employee = employeeRepository.findAllById(empid);
+            if (employee != null)
+                employeeRepository.deleteById(empid);
             return new ResponseEntity<>(HttpStatus.OK);
-        } catch (Exception e) {
-            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
         }
+        return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+
+    } catch(
+    Exception e)
+
+    {
+        return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
     }
 
-    public ResponseEntity<Object> deleteEmployeeById(int id) {
-    }
+}
 
-    public ResponseEntity<EmployeeDto> updateId(int id, EmployeeDto employeeDto) {
-        //check if employee exist in database
-        EmployeeRepository poObj = getEmpRec(empid);
 
-        if (poObj != null) {
-            poObj.setempid(EmployeeDto.getempid());
-            poObj.setname(EmployeeDto.getname());
-            poObj.setaddress(EmployeeDto.getaddress());
-            poObj.setphone(EmployeeDto.getphone());
-            poObj.setage(EmployeeDto.getage());
-            poObj.setdob(EmployeeDto.getdob());
-            poObj.setnic(EmployeeDto.getnic());
-            return new ResponseEntity<>(EmployeeRepository.save(poObj), HttpStatus.OK);
+    public ResponseEntity<Object> updateEmployee(int empid, Employee employee) {
+        Optional<Employee> employeeObj = employeeRepository.findById(empid);
+        Employee newEmployee = employeeObj.get();
+
+        if (employeeObj != null) {
+            newEmployee.setName(employee.getName());
+            newEmployee.setAddress(employee.getAddress());
+            newEmployee.setPhone(employee.getPhone());
+            newEmployee.setAge(employee.getAge());
+            newEmployee.setDOB(employee.getDOB());
+            newEmployee.setNic(employee.getNic());
+            return new ResponseEntity<>(EmployeeRepository.save(newEmployee), HttpStatus.OK);
         }
 
         return new ResponseEntity<>(HttpStatus.NOT_FOUND);
